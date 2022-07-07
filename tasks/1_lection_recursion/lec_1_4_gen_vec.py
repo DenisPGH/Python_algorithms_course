61e04e892c3c3adbeda052aacdb4683a43c60082
Denislav <deni_9000_1990@abv.bg>
2022-07-07 17:47:57 +0300

    calculation variacii

--- tasks/1_lection_recursion/lec_1_4_gen_vec.py
+++ tasks/1_lection_recursion/lec_1_4_gen_vec.py
@@ -10,18 +10,31 @@
 # number=int(input())
 # vector=[None]*number
 # gen_vectors_1_0(0,vector)
+import time
+
+class Counter:
+    def __init__(self):
+        self.VALUE=0
+        self.RANGE=49
+        self.VECTOR=6
+
+c=Counter()
 
 
 def gen_vectors_1_0(index,vector):
     if index >=len(vector):
-        print(*vector,sep='')
+        #print(*vector,sep='')
+        c.VALUE+=1
         return
-    for num in range(2):
+    for num in range(1,c.RANGE):
         vector[index]=num
         gen_vectors_1_0(index+1,vector)
 
 
-number=int(input())
-vector=[None]*number
+
+
+vector=[None]*c.VECTOR
+start=time.time()
 gen_vectors_1_0(0,vector)
 
+print(f"all variation= {c.VALUE}, time={(time.time()-start):.3f} sec")
