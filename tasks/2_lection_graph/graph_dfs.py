ecff8848d6caf219137c537fba654d34c357141a
Denislav <deni_9000_1990@abv.bg>
2022-07-21 08:57:25 +0300

    lec 2 , graph dfs

---
+++ tasks/2_lection_graph/graph_dfs.py
@@ -0,0 +1,34 @@
+def dfs(node, graph, visited):
+    if node in visited:
+        return
+    visited.append(node)
+    for kids in graph[node]:
+        dfs(kids, graph, visited)
+
+    return visited
+
+
+
+
+
+graph={
+    1:[2,3,4],
+    2:[3],
+    3:[],
+    4:[],
+    5:[]
+}
+
+
+
+visited=[]
+
+
+
+
+
+for node in graph:
+    a=dfs(node,graph,visited)
+    if a:
+        print(a)
+
