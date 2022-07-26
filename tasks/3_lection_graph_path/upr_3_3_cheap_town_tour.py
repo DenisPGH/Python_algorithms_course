c8cef8ae643fc524770f25c4952f0cf3660fe3b9
Denislav <deni_9000_1990@abv.bg>
2022-07-26 14:18:55 +0300

    upr 3 task 3 version 1

---
+++ tasks/3_lection_graph_path/upr_3_3_cheap_town_tour.py
@@ -0,0 +1,95 @@
+from collections import  deque
+from queue import PriorityQueue
+
+
+class Edge:
+    def __init__(self,source,destination,weight):
+        self.source = source
+        self.destination = destination
+        self.weight = weight
+
+def my_input_data_to_graph(edges):
+    input_=[['0 - 1 - 10'],
+            ['0 - 2 - 6'],
+            ['0 - 3 - 5'],
+            ['1 - 3 - 15'],
+            ['2 - 3 - 4']
+            ]
+    graph={}
+
+    for index in range(len(input_)):
+        source, destination, weight = [int(y) for y in input_[index][0].split(' - ')]
+        if source not in graph:
+            graph[source] = []
+        if destination not in graph:
+            graph[destination] = []
+        graph[source].append(Edge(source, destination, weight))
+        graph[destination].append(Edge(source, destination, weight))
+
+    return graph
+
+def store_data_to_graph(edges):
+    graph={}
+
+    for _ in range(edges):
+        source, destination, weight = [int(x) for x in input().split(" - ")]
+        if source not in graph:
+            graph[source] = []
+        if destination not in graph:
+            graph[destination] = []
+        graph[source].append(Edge(source, destination, weight))
+        graph[destination].append(Edge(source, destination, weight))
+
+    return graph
+
+def find_all_ways_cheapest(start,target,graph):
+
+    distances = {}
+    parents = {}
+    visited = {}
+    for nod in graph:
+        distances[nod] = float('inf')
+        parents[nod] = None
+        visited[nod] = False
+
+    distances[start]=0 # declare the start node with distance 0
+    pq=PriorityQueue()
+    pq.put((0,start))
+    while not pq.empty():
+        min_distance_to_the_node,node=pq.get() # get the min value in the queue
+        if node ==target:
+            break
+        for edge in graph[node]:
+            child = edge.destination if edge.source == node else edge.source
+            new_distance= min_distance_to_the_node+edge.weight
+            if new_distance < distances[child]:
+                distances[child]=new_distance
+                parents[child]=node
+                pq.put((new_distance,child))
+    return distances,parents
+
+
+towns=4 #int(input()) #
+streets=5 #int(input()) #
+graph=my_input_data_to_graph(streets)
+#print(graph)
+dict_min_value_each_town={}
+
+for town in graph:
+    print(town)
+    from_town=town
+    to_town=3
+
+    costs,parents=find_all_ways_cheapest(from_town,to_town,graph)
+    for each_town in costs:
+        if each_town not in dict_min_value_each_town:
+            dict_min_value_each_town[each_town] = float('inf')
+        if dict_min_value_each_town[each_town]>costs[each_town] and costs[each_town] !=0:
+            dict_min_value_each_town[each_town]=costs[each_town]
+    print(costs)
+    print(sum(costs.values()))
+
+print(dict_min_value_each_town)
+
+
+
