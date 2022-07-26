cb3668aae979fff7a09e3cff22cf44fc0d712e96
Denislav <deni_9000_1990@abv.bg>
2022-07-26 14:54:35 +0300

    upr 3 task 3 version 2

--- tasks/3_lection_graph_path/upr_3_3_cheap_town_tour.py
+++ tasks/3_lection_graph_path/upr_3_3_cheap_town_tour.py
@@ -8,6 +8,18 @@ class Edge:
         self.destination = destination
         self.weight = weight
 
+def generate_path_source_to_target(target_,parents):
+    """
+    1.this function got the target and parents lists
+    2.return the shortes path to the target
+     """
+    path = deque()
+    node = target_
+    while node is not None:
+        path.appendleft(node)
+        node = parents[node]
+    return path
+
 def my_input_data_to_graph(edges):
     input_=[['0 - 1 - 10'],
             ['0 - 2 - 6'],
@@ -25,6 +37,7 @@ def my_input_data_to_graph(edges):
             graph[destination] = []
         graph[source].append(Edge(source, destination, weight))
         graph[destination].append(Edge(source, destination, weight))
+        #graph[destination].append(Edge( destination,source, weight))
 
     return graph
 
@@ -55,17 +68,29 @@ def find_all_ways_cheapest(start,target,graph):
     distances[start]=0 # declare the start node with distance 0
     pq=PriorityQueue()
     pq.put((0,start))
+    visited[start]=True
+    cycle=False
     while not pq.empty():
         min_distance_to_the_node,node=pq.get() # get the min value in the queue
-        if node ==target:
-            break
         for edge in graph[node]:
+            if all([x for x in visited.values()]):
+                cycle=True
+                break
             child = edge.destination if edge.source == node else edge.source
+
             new_distance= min_distance_to_the_node+edge.weight
             if new_distance < distances[child]:
                 distances[child]=new_distance
                 parents[child]=node
+                visited[child]=True
                 pq.put((new_distance,child))
+                print(visited)
+
+
+
+        if cycle:
+            # got cycle
+            break
     return distances,parents
 
 
@@ -73,23 +98,22 @@ towns=4 #int(input()) #
 streets=5 #int(input()) #
 graph=my_input_data_to_graph(streets)
 #print(graph)
-dict_min_value_each_town={}
+dict_min_value_pair={}
 
 for town in graph:
     print(town)
     from_town=town
-    to_town=3
-
+    to_town=town
     costs,parents=find_all_ways_cheapest(from_town,to_town,graph)
-    for each_town in costs:
-        if each_town not in dict_min_value_each_town:
-            dict_min_value_each_town[each_town] = float('inf')
-        if dict_min_value_each_town[each_town]>costs[each_town] and costs[each_town] !=0:
-            dict_min_value_each_town[each_town]=costs[each_town]
-    print(costs)
-    print(sum(costs.values()))
 
-print(dict_min_value_each_town)
+    print(costs)
+
+    #print(sum(costs.values()))
+    #print(parents)
+
+
+
+
 
 
 
